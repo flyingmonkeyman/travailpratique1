3441bb7596cf5b577f21b8e12feeb804fa4a7618
LP <gastonmigner@gmail.com>
2020-03-04 10:46:14 -0500

    ajout initial

---
+++ keymon/keymon.cpp
@@ -0,0 +1,9 @@
+// keymon.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello monkeys!\n";
+}
